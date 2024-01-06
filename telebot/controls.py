e7186c11e8c20b6cb588ed4f4f10e9f02878bf74
viktoria023 <62943294+viktoria023@users.noreply.github.com>
2024-01-06 16:15:02 +0100

    trying a different setup

---
+++ telebot/controls.py
@@ -0,0 +1,8 @@
+def get_response(msg):
+    """
+    This function will return a response based on the message text.
+
+    can be extended to use machine learning or other AI concepts to generate sophisticated responses
+    this is the bare minimum.
+    """
+    return "test"
