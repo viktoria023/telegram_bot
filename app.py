6b9cfa317b641fb0efa272cf395cf2fee053a86f
viktoria023 <62943294+viktoria023@users.noreply.github.com>
2024-01-08 19:05:31 +0100

    code cleanup

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 import re
 from flask import Flask, request
 import telegram
-from telebot.credentials import bot_token, bot_user_name, URL
+from telebot.credentials import bot_token, URL
 from telebot.controls import get_response
 
 global bot
@@ -33,32 +33,6 @@ async def respond():
     # now just send the message back
     # notice how we specify the chat and the msg we reply to
     await bot.sendMessage(chat_id=chat_id, text=response, reply_to_message_id=msg_id)
-
-    # # the first time you chat with the bot AKA the welcoming message
-    # if text == "/start":
-    #     # print the welcoming message
-    #     bot_welcome = """
-    #    Welcome to coolAvatar bot, the bot is using the service from http://avatars.adorable.io/ to generate cool looking avatars based on the name you enter so please enter a name and the bot will reply with an avatar for your name.
-    #    """
-    #     # send the welcoming message
-    #     bot.sendMessage(chat_id=chat_id, text=bot_welcome, reply_to_message_id=msg_id)
-
-    # else:
-    #     try:
-    #         # clear the message we got from any non alphabets
-    #         text = re.sub(r"\W", "_", text)
-    #         # create the api link for the avatar based on http://avatars.adorable.io/
-    #         url = "https://api.adorable.io/avatars/285/{}.png".format(text.strip())
-    #         # reply with a photo to the name the user sent,
-    #         # note that you can send photos by url and telegram will fetch it for you
-    #         bot.sendPhoto(chat_id=chat_id, photo=url, reply_to_message_id=msg_id)
-    #     except Exception:
-    #         # if things went wrong
-    #         bot.sendMessage(
-    #             chat_id=chat_id,
-    #             text="There was a problem in the name you used, please enter different name",
-    #             reply_to_message_id=msg_id,
-    #         )
 
     return "ok"
 
--- telebot/controls.py
+++ telebot/controls.py
@@ -7,8 +7,7 @@ def get_response(msg):
     """
     This function will return a response based on the message text.
 
-    can be extended to use machine learning or other AI concepts to generate sophisticated responses
-    this is the bare minimum.
+    can be extended to use machine learning or other AI concepts to generate sophisticated responses.
     """
 
     if msg == "/start":
