524816228d2813645a660a5b44707e49c58e12e7
Viktoria Brunner <62943294+viktoria023@users.noreply.github.com>
2024-02-07 15:02:49 +0000

    remove unused commands

--- telebot/credentials.py
+++ telebot/credentials.py
@@ -1,5 +1,3 @@
 bot_token = "***"
 bot_user_name = "Littlest_helper_bot"
 URL = "***"
-# "https://348f-2003-eb-172a-d00-2595-a9be-ac7-88e1.ngrok-free.app"
-# "https://chat-bot-2-8468cdaba27b.herokuapp.com/"
